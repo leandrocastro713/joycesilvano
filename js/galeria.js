b4cb8082e5f11a7a2f84ef4155f5b3d19dd5f628
leandrocastro713 <109395934+leandrocastro713@users.noreply.github.com>
2022-07-28 14:07:59 -0300

    ./ No lugar de ../

--- js/galeria.js
+++ js/galeria.js
@@ -9,14 +9,14 @@ let novaImg = null;
 
 for(let i = 1; i<91; i++){
     if(i<10){
-        tempArq = '../galeria/foto00' + i + '.jpeg';
+        tempArq = './galeria/foto00' + i + '.jpeg';
         novaImg = document.createElement('img');
         novaImg.src = tempArq;
         novaImg.className = 'imagem';
         ondeInserir.appendChild(novaImg);
     }
     if(i>=10 && i<91){
-        tempArq = '../galeria/foto0' + i + '.jpeg';
+        tempArq = './galeria/foto0' + i + '.jpeg';
         novaImg = document.createElement('img');
         novaImg.src = tempArq;
         novaImg.className = 'imagem';
