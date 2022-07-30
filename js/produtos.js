e3aeb202167ea6cfbd0d72270d5f1a37af69a27d
leandrocastro713 <109395934+leandrocastro713@users.noreply.github.com>
2022-07-29 21:36:18 -0300

    css + imagens + js

---
+++ js/produtos.js
@@ -0,0 +1,6 @@
+divProduto = document.querySelectorAll('.produto');
+qtdDivs = document.querySelectorAll('.produto').length-1;
+console.log(divProduto, qtdDivs);
+// for(let i=0; qtdDivs; i++){
+//     console.log(qtdDivs, divProduto[i]);
+}
