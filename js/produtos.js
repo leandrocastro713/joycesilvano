2ef497aef38bb58c98137ab06bc52fae061d1623
leandrocastro713 <109395934+leandrocastro713@users.noreply.github.com>
2022-08-02 00:25:10 -0300

    atualização em 1/7

--- js/produtos.js
+++ js/produtos.js
@@ -1,6 +1,165 @@
-divProduto = document.querySelectorAll('.produto');
-qtdDivs = document.querySelectorAll('.produto').length-1;
-console.log(divProduto, qtdDivs);
-// for(let i=0; qtdDivs; i++){
-//     console.log(qtdDivs, divProduto[i]);
+function filtro_abela(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('abela')!=-1){
+      console.log('achou abela');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou abela')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+function filtro_anazoe(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('anazoe')!=-1){
+      console.log('achou anazoe');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou anazoe')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+function filtro_aneethun(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('aneethun')!=-1){
+      console.log('achou aneethun');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou aneethun')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+function filtro_apse(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('apse')!=-1){
+      console.log('achou apse');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou apse')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+function filtro_arvensis(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('arvensis')!=-1){
+      console.log('achou arvensis');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou arvensis')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+function filtro_arvensis(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('arvensis')!=-1){
+      console.log('achou arvensis');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou arvensis')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+function filtro_acessorios(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('acessorios')!=-1){
+      console.log('achou acessorios');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou acessorios')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+function filtro_curlycare(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('curlycare')!=-1){
+      console.log('achou curlycare');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou curlycare')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+
+function filtro_widi(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+    console.log(i, divProduto[i], divProduto[i].className)
+    if(divProduto[i].className.indexOf('widi')!=-1){
+      console.log('achou widi');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+      } else{
+      console.log('não achou widi')
+      divProduto[i].classList.add('hide');
+      console.log(divProduto[i].className);
+    }
+  }
+}
+
+
+function filtro_todos(){
+  var divProduto = document.querySelectorAll('.produto');
+  var qtdDivs = divProduto.length-1;
+  for(let i=0; qtdDivs; i++){
+      console.log(i, divProduto[i], divProduto[i].className)
+      console.log('achou todos');
+      divProduto[i].classList.remove('hide');
+      console.log(divProduto[i].className);
+  }
 }
